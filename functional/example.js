31c1e5109820ef514465d77ce96b9b507378e64b
Prem Tiwari <prem.tiwari@news.co.uk>
2020-08-04 09:56:35 +0530

    added functional test

---
+++ functional/example.js
@@ -0,0 +1,13 @@
+describe('WP Login test', () => {
+    it('successfully loads', () => {
+      cy.visit('http://test:8888/liveblog/wp-admin/') 
+
+      cy.get('#user_login').type('superadmin')
+      cy.get('#user_pass').type('superadmin')
+
+      cy.get('#wp-submit').click()
+
+      //Add new article
+      cy.get('').click()
+    })
+  })
